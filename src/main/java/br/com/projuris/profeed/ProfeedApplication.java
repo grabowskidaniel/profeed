9174f8fa30ef1cf0aba870c8a9ec1957871ded98
erick.oliveira <erick.oliveira@projuris.com.br>
2019-05-09 14:57:03 -0300

    first commit

---
+++ src/main/java/br/com/projuris/profeed/ProfeedApplication.java
@@ -0,0 +1,13 @@
+package br.com.projuris.profeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfeedApplication.class, args);
+    }
+
+}
